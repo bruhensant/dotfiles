37048012663901a2b796bfbfdb6ad895c2c43452
Bruno Oliveira <bruhensant@gmail.com>
2024-02-16 21:58:29 -0300

    feat(*): muita coisa ao mesmo tempo

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,1 +1,32 @@
-alias ccze="EDITOR=code chezmoi edit"
+# Arquivo externo de aliases 
+
+## Comandos
+alias ll="ls -la"
+alias rm="rm -i"
+
+## Encurtadores
+alias cls="clear"
+alias omr="omz reload"
+
+## Git
+alias gb="gh f -b"
+alias gfl="gf && gl"
+
+# Edição
+alias mzdt="micro ~/.zshrc"
+alias mi3dt="micro ~/.config/i3/config"
+
+## Substituições
+alias man="batman"
+alias l="eza"
+alias cat="bat"
+alias htop="btop"
+alias ls="eza"
+alias cd="z"
+
+# Chezmoi
+alias cze="EDITOR=code chezmoi edit"
+alias cza="chezmoi apply"
+alias czaa="chezmoi add"
+alias cc="chezmoi cd"
+alias cz="chezmoi"
